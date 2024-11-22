186c6a958d8c7d17bb36d0abcc8d505158059824
Janak Suryabanshi <suryabanshi.jnk@gmail.com>
2024-11-22 19:54:42 +0545

    reverting back

--- apps/app/src/App.tsx
+++ apps/app/src/App.tsx
@@ -1,16 +1,15 @@
-// import { CssBaseline, ThemeProvider } from '@mui/material';
-// import { RouterProvider } from 'react-router-dom';
-// import { theme } from '@repo/ui';
-// import { router } from './routes';
-// import './App.css';
+import { CssBaseline, ThemeProvider } from '@mui/material';
+import { RouterProvider } from 'react-router-dom';
+import { theme } from '@repo/ui';
+import { router } from './routes';
+import './App.css';
 
 function App() {
   return (
-    // <ThemeProvider theme={theme}>
-    //   <CssBaseline />
-    //   <RouterProvider router={router} />
-    // </ThemeProvider>
-    <div>Hello World</div>
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <RouterProvider router={router} />
+    </ThemeProvider>
   );
 }
 
