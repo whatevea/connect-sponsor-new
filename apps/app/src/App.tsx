d1c7a23496034878c40a62d2ac34212dcad3b810
Janak Suryabanshi <suryabanshi.jnk@gmail.com>
2024-11-22 19:05:35 +0545

    testing

--- apps/app/src/App.tsx
+++ apps/app/src/App.tsx
@@ -1,15 +1,16 @@
-import { CssBaseline, ThemeProvider } from '@mui/material';
-import { RouterProvider } from 'react-router-dom';
-import { theme } from '@repo/ui';
-import { router } from './routes';
-import './App.css';
+// import { CssBaseline, ThemeProvider } from '@mui/material';
+// import { RouterProvider } from 'react-router-dom';
+// import { theme } from '@repo/ui';
+// import { router } from './routes';
+// import './App.css';
 
 function App() {
   return (
-    <ThemeProvider theme={theme}>
-      <CssBaseline />
-      <RouterProvider router={router} />
-    </ThemeProvider>
+    // <ThemeProvider theme={theme}>
+    //   <CssBaseline />
+    //   <RouterProvider router={router} />
+    // </ThemeProvider>
+    <div>Hello World</div>
   );
 }
 
