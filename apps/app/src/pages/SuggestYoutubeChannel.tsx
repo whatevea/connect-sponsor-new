5a5b11397981374816cb4f033163687920dc3d03
Janak Suryabanshi <suryabanshi.jnk@gmail.com>
2024-11-28 14:55:12 +0545

    Added 3 components

--- apps/app/src/pages/SuggestYoutubeChannel.tsx
+++ apps/app/src/pages/SuggestYoutubeChannel.tsx
@@ -3,7 +3,9 @@ import { Typography } from '@mui/material';
 export default function SuggestYoutubeChannelPage() {
   return (
     <div>
-      <Typography variant='h5'>Suggest Youtube Channel Page</Typography>
+      <Typography variant='h5'>Suggest Youtube Channels </Typography>
+      <Typography sx={{marginY:2}}>Suggest a YouTube channel you'd like us to track and stay updated on. Simply provide the channel's name, URL, and category.</Typography>
+    
     </div>
   );
 }
