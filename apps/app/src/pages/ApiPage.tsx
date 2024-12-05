e5d5ab04504529496c455d06095b8ae13e82ab95
Anjana789 <anjanachuhan2057@gmail.com>
2024-12-05 10:46:20 +0000

    fixing ui of api, reseller and price page

--- apps/app/src/pages/ApiPage.tsx
+++ apps/app/src/pages/ApiPage.tsx
@@ -11,22 +11,24 @@ export default function ApiPage() {
 
         <Box
             sx={{
-                width: '66.666%',
+                width: '60%',
                 display: 'flex',
                 gap: 2,
                 borderRadius: 4,
                 border: '2px solid white',
                 background: 'linear-gradient(to right, rgba(56, 38, 185, 0.42), transparent)',
-                paddingY: 4,
-                paddingX: 2,
-                color: 'white'
+                paddingY: 3,
+                paddingBottom: 3,
+                paddingX: 3,
+                color: 'white',
+                marginTop: 8,
             }}
         >
             <Box>
                 <Avatar
                     sx={{
-                        width: 96,
-                        height: 96,
+                        width: 78,
+                        height: 78,
                         backgroundColor: 'rgba(255, 255, 255, 0.2)',
                         display: 'flex',
                         alignItems: 'center',
@@ -34,46 +36,46 @@ export default function ApiPage() {
                     }}
                 >
                     <img
-                        src="https://placeholder.co/48x38/FFA51F/white"
+                        src="/icons/images/closing_tag.png"
                         alt="closingTag.icon"
                         style={{
                             borderRadius: 4,
                             width: 48,
-                            height: 38
+                            height: 32
                         }}
                     />
                 </Avatar>
             </Box>
 
-            <Box sx={{ display: 'flex', flexDirection: 'column', gap: 2 }}>
+            <Box sx={{ display: 'flex', flexDirection: 'column', gap: 2.3, paddingTop: 0.9 }}>
                 <Typography
-                    variant="h3"
+                    variant="h4"
                     sx={{
-                        fontSize: '2rem',
-                        fontWeight: 'bold'
+                        fontWeight: 'bold',
                     }}
                 >
                     Sponsorships API for Developers
                 </Typography>
 
                 <Typography
-                    variant="body2"
                     sx={{
-                        fontSize: '0.875rem',
+                        fontSize: 15,
                         lineHeight: 1.5,
+                        fontWeight: 300,
+                        paddingRight: 3,
                     }}
                 >
-                    ConnectSponsors.com offers a public API that lets you directly import your chosen sponsorship leads into your CRM or outreach tool. To start building, upgrade to our Enterprise Plan and create your API key below.
+                    ConnectSponsors.Com Offers A Public API That Lets You Directly Import Your Chosen Sponsorship Leads Into Your CRM Or Outreach Tool. To Start Building, Upgrade To Our Enterprise Plan And Create Your API Key Below.
                 </Typography>
 
                 <Typography
-                    variant="h6"
                     sx={{
-                        fontSize: '1.125rem',
-                        fontWeight: 'semibold'
+                        fontSize: '1.1rem',
+                        fontWeight: 600,
+                        paddingBottom: 0.5
                     }}
                 >
-                    For Technical Information, please look at our API documentation.
+                    For Technical Information, Please look At Our API Documentation.
                 </Typography>
                 <Box>
                     <Button
@@ -81,7 +83,7 @@ export default function ApiPage() {
                         sx={{
                             borderRadius: 6,
                             backgroundColor: '#E9901A',
-                            padding: '0.5rem 1.5rem',
+                            padding: '0.5rem 1.3rem',
                             fontSize: '1.125rem',
                             '&:hover': {
                                 backgroundColor: '#D88019'
